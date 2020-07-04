02f56e4e2d4061f538f54a171ba346381df2520d
TylerCEdge <tylercedge@gmail.com>
2020-07-04 16:20:22 -0400

    post service tested successfully with postman for GET & POST routes.

--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
+app.use(bodyParser.json());
 
 const posts = {};
 
@@ -11,6 +13,13 @@ app.get('/posts', (req, res) => {
 
 app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
